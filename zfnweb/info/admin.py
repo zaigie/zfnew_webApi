2d589eb2ba5bdc10deef79b2d0ce20025ef94f88
JoyceWedamson <jokerwho@yeah.net>
2020-06-15 10:38:54 +0800

    admin增加修改页分段

--- zfnweb/info/admin.py
+++ zfnweb/info/admin.py
@@ -8,6 +8,14 @@ admin.site.site_header="西院助手后台管理"
 admin.site.index_title="欢迎来到西院助手后台管理"
 
 class StuAdmin(admin.ModelAdmin):
+    ordering = ('-updateTime',)
+    fieldsets = (
+        ("基本信息", {'fields': ['studentId', 'name', 'gpa']}),
+        ("院系班级", {'fields':['collegeName', 'majorName', 'className']}),
+        ("额外信息", {'fields':['phoneNumber', 'birthDay']}),
+        ("登录相关", {'fields':['updateTime', 'refreshTimes']}),
+        ("Cookies", {'fields':['JSESSIONID', 'route']}),
+    )
     list_display = ('studentId','name','collegeName','majorName','className','phoneNumber','birthDay','updateTime')
     search_fields = ('studentId','name','collegeName','majorName','className','phoneNumber','birthDay')
     list_filter = ('collegeName','majorName','className')
